de9b148d749d40a426724ea85de760270f0353ac
Andy Stanton <andystanton@users.noreply.github.com>
2021-11-05 09:39:08 +0000

    fix and improve test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -13,16 +13,17 @@ make dockerbuild
 
 echo "Running tests..."
 
-makefile_output=$(make docker 2>dev/null)
+expected_output="Hello world"
+makefile_output=$(make docker 2>/dev/null)
 
 echo "Test results..."
 
 exit_code=1
-if [ $? = 0 ] && echo "$makefile_output" | egrep -q '^Hello world$'; then
-    printf "[\x1b[32m✔\x1b[0m] Test: Hello world\n"
+if [ $? = 0 ] && echo "$makefile_output" | egrep -q '^'"${expected_output}"'$'; then
+    printf "[\x1b[32m✔\x1b[0m] Test: ${makefile_output}\n"
     exit_code=0
 else
-    printf "[\x1b[31m✘\x1b[0m] Test: Hello world\n"
+    printf "[\x1b[31m✘\x1b[0m] Test: Expected: ${expected_output}, Actual: ${makefile_output}\n"
 fi
 
 echo "Cleaning up Docker image"
